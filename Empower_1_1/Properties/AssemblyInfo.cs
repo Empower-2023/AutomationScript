adb7de102ec25b5f7a8e337247e738e0a785a854
Rajesh R <rajesh.r@skyline.be>
2023-06-04 12:35:03 +0800

    Add project files.

---
+++ Empower_1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7ECCEE3B-B480-46E7-899C-37853803DF9B")]
+[assembly: AssemblyVersion("1.0.0.0")]
